fc28f9759dae8269d2962a71b7e616923f3aca7d
Tony <tonyanziano5@gmail.com>
2025-04-27 20:13:08 -0700

    Added publishing support with electron-forge

--- forge.config.ts
+++ forge.config.ts
@@ -31,6 +31,18 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github', 
+      config: {
+        repository: {
+          owner: 'tonyanziano',
+          name: 'accent-helper-electronjs',
+        },
+        prerelease: true,
+      }
+    }
+  ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
